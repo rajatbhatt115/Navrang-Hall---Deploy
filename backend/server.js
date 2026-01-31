3cf9aa7cb3f119baf678c50cd4aa96d1d5465f96
Rajat Bhatt <rajatnbhatt1994@gmail.com>
2026-02-01 00:44:03 +0530

    Fix db.json path for Render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./data/db.json')
+const path = require('path');
+
+const router = jsonServer.router(
+  path.join(__dirname, 'data', 'db.json')
+);
 const middlewares = jsonServer.defaults()
 const cors = require('cors')
 
